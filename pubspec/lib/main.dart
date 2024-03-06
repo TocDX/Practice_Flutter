033883a93cec708549c09f0510ceac77f0acfa10
TocDx <sk372021000@gmail.com>
2024-03-06 23:22:23 +0900

    commit

---
+++ pubspec/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomeWidget(),
+    ),
+  );
+}
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        body: Body(),
+      ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+
+      ],
+    );
+  }
+}
+
